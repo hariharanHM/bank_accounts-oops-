d9e70f841a94abdeb7cbb558fe9f2951de73800a
HariHaran C <74068415+hariharanHM@users.noreply.github.com>
2021-10-07 20:48:12 +0530

    Update accounts.java

--- accounts.java
+++ accounts.java
@@ -8,6 +8,7 @@ public class accounts {
 
     public  accounts(){
         System.out.println("Empty constructor called");
+        System.out.println("this is oops project for bankaccount")
     }
     public accounts(String number,double balance,String customerName,String customerEmailAddress,String customerPhoneNumber){
         System.out.println("Account constructor with paramters called");
